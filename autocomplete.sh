d900bf2ee193ec6da5039d469cb00a60c3a0d36f
Arturo254 <87346871+Arturo254@users.noreply.github.com>
2022-08-29 14:51:26 -0500

    Add files via upload

---
+++ autocomplete.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+
+#TOOL CRATED BY ARTURO
+
+
+
+
+
+	clear
+
+
+	 cd /data/data/com.termux/files/home/autocomplete
+
+		 lolcat box.cow 
+ 
+			sleep 2
+
+				clear 
+
+					figlet -f future Comenzando | lolcat
+   
+	
+						curl -L https://get.oh-my.fish | fish
+
+							omf install ocean 
+
+
+							    sleep 3 
+
+									figlet -f future Finalizado  | lolcat
+
+
+										clear && bash
